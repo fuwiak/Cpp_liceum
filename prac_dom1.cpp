1c2aee4c530ff0480b2fe8674bf6885b877ff3f4
Pawel <poczta130@gmail.com>
2020-03-18 20:01:47 +0100

    Create prac_dom1.cpp

---
+++ prac_dom1.cpp
@@ -0,0 +1,4 @@
+
+//zad1 Napisz program, który oblicza maximum z dwoch podanych liczb
+//zad2 Napisz program, który sprawdza parzystosc podanej liczby całkowitej
+//zad3 Napisz program, ktory wyznacza pole trojkata dla zadanej podstawy o dlugosci a i wysokosci h
