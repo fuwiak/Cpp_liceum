e3bfc66b7c0e791f9986560a50795a39e196f269
Pawel <poczta130@gmail.com>
2020-03-18 18:35:09 +0100

    Create for_while.cpp

---
+++ for_while.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+
+
+
+int main(){
+
+int i;
+
+for(i=0;i<10;i++){
+    std::cout << i << std::endl;
+    }
+    
+int x=0;
+
+while(x<10){
+  std::cout << x << std::endl;
+  x++;
+ 
+
+
+
+
+
+return 0;
+}
