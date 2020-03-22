513aeed8405bb62e231ed9ff52d58a31d0e5c6c1
Pawel <poczta130@gmail.com>
2020-03-22 12:55:47 +0100

    Update prac_dom1.cpp

--- prac_dom1.cpp
+++ prac_dom1.cpp
@@ -1,4 +1,21 @@
 
 //zad1 Napisz program, który oblicza maximum z dwoch podanych liczb
+
+int maksi(int a, int b){
+  if (a>=b){
+    return a;
+  }
+  else{
+    return b;
+  }
+   
+}
+
+
+std::cout << maksi(1,3) << std::endl;
+
+
+
+
 //zad2 Napisz program, który sprawdza parzystosc podanej liczby całkowitej
 //zad3 Napisz program, ktory wyznacza pole trojkata dla zadanej podstawy o dlugosci a i wysokosci h
