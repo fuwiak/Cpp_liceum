72770286058b90b608ffc8b846419fe6e831bea5
Pawel <poczta130@gmail.com>
2020-04-04 14:16:33 +0200

    Create part1.py

---
+++ part1.py
@@ -0,0 +1,54 @@
+# most basic data dtypes
+
+x=3 #int
+
+s = "string" #str
+
+f = 1.1 #float
+
+#check out the result:
+'''
+x = x*2
+
+s=s*2
+
+f=*2
+
+'''
+
+#if,elif, else
+
+delta=b**2 -4*a*c
+
+if delta>0:
+  print("Two real solutions")
+elif delta==0:
+  print("One real solution")
+else:
+  print("No real solution")
+  
+# for, while
+
+for i in range(1, 10):
+  print(i)
+  
+ind=1
+while(ind<10):
+  print(ind)
+  ind+=1
+
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
